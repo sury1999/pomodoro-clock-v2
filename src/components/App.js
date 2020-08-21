3fc97b7e2c8871125f73846c9054c1df67962dcb
sury1999 <suryansusengupta@gmail.com>
2020-08-22 01:50:49 +0530

    cleaning up finished

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <main>
+        <h2>Pomodoro Clock</h2>
+      </main>
+    );
+  }
+}
+
+export default App;
